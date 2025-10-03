5bbff0adc02188192490e591ec7d49f761af3d90
Rhodless <ghusilver@gmail.com>
2025-10-03 18:11:24 +0200

    build: initial Gradle project setup with Kotlin and Ktor dependencies

---
+++ backend/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.22"
+    application
+    id("io.ktor.plugin") version "2.3.7"
+}
+
+group = "com.epitech.area"
+version = "1.0.0"
+
+application {
+    mainClass.set("com.epitech.area.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val ktorVersion = "2.3.7"
+    val kotlinVersion = "1.9.22"
+    val logbackVersion = "1.4.14"
+    val mongoVersion = "4.11.1"
+    val redisVersion = "5.1.1"
+    val rabbitMQVersion = "5.20.0"
+    val jwtVersion = "4.4.0"
+
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors:$ktorVersion")
+    implementation("io.ktor:ktor-server-rate-limit:$ktorVersion")
+    implementation("io.ktor:ktor-server-websockets:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-compression:$ktorVersion")
+    implementation("io.ktor:ktor-server-caching-headers:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:$mongoVersion")
+    implementation("org.mongodb:bson-kotlinx:$mongoVersion")
+
+    implementation("redis.clients:jedis:$redisVersion")
+
+    implementation("com.rabbitmq:amqp-client:$rabbitMQVersion")
+
+    implementation("com.auth0:java-jwt:$jwtVersion")
+
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("de.mkammerer:argon2-jvm:2.11")
+
+    implementation("com.google.api-client:google-api-client:2.2.0")
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation("com.google.apis:google-api-services-gmail:v1-rev20240520-2.0.0")
+    implementation("com.google.apis:google-api-services-drive:v3-rev20240521-2.0.0")
+    implementation("javax.mail:javax.mail-api:1.6.2")
+    implementation("com.sun.mail:javax.mail:1.6.2")
+
+    implementation("org.kohsuke:github-api:1.318")
+
+    implementation("com.slack.api:slack-api-client:1.36.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation("org.testcontainers:testcontainers:1.19.3")
+    testImplementation("org.testcontainers:mongodb:1.19.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
